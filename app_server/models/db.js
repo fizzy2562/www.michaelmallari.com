232f608715be0f7c6a7248a81297f538638669c4
Michael Mallari <michael@michaelmallari.com>
2016-09-30 10:38:58 -0400

    Detect environment, define database connection, and display status

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,55 @@
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
+var gracefulShutdown;
+
+
+// Detect environment and define database connection string and use it to open Mongoose connection
+var dbURI = 'mongodb://localhost/www-michaelmallari-com';
+if(process.env.NODE_ENV === 'production') {
+	dbURI = process.env.MLAB_URI;
+}
+mongoose.connect(dbURI);
+
+
+// Listen for Mongoose connection events and output statuses to console
+mongoose.connection.on('connected', function() {
+	console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+	console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+	console.log('Mongoose disconnected');
+});
+
+
+// Reusable function to close Mongoose connection
+gracefulShutdown = function(msg, callback) {
+	mongoose.connection.close(function() {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+}
+
+
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+	gracefulShutdown('nodemon restart', function() {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+
+
+// For app termination
+process.on('SIGINT', function() {
+	gracefulShutdown('app termination', function() {
+		process.exit(0);
+	});
+});
+
+
+// For Heroku app termination
+process.on('SIGTERM', function() {
+	gracefulShutdown('Heroku app shutdown', function() {
+		process.exit(0);
+	});
+});
