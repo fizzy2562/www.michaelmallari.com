07b3fd625736cb1a56ee72a3dde5d4241590321b
Michael Mallari <michael@michaelmallari.com>
2016-10-03 23:24:21 -0400

    Added .gitignore + initial styles

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,11 @@
 module.exports.home = function(req, res, next) {
-	res.render('index', { title: 'Michael Mallari, MBA, CSPO, CSM' });
+	res.render('index', {
+		html: {
+			head: {
+				title: 'Michael Mallari, MBA, CSPO, CSM',
+				description: 'Hybrid product manager, designer, and developer. Launched digital products for American Express, Ford, Hasbro, Marriott, Marvel, Ralph Lauren, and others.',
+				canonical: 'http://www.michaelmallari.com'
+			}
+		}
+	});
 };
