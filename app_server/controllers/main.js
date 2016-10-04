ad17b8eeb7746492b8998623d798b36846c910fb
Michael Mallari <michael@michaelmallari.com>
2016-10-04 15:11:54 -0400

    Added header and footer styles

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,8 +1,8 @@
 module.exports.home = function(req, res, next) {
-	res.render('index', {
+	res.render('home', {
 		html: {
 			head: {
-				title: 'Michael Mallari, MBA, CSPO, CSM',
+				title: 'Michael Mallari, MBA, CSPO, CSM | Hybrid Thinker and Doer',
 				description: 'Hybrid product manager, designer, and developer. Launched digital products for American Express, Ford, Hasbro, Marriott, Marvel, Ralph Lauren, and others.',
 				canonical: 'http://www.michaelmallari.com'
 			}
