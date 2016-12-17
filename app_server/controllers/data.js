814ed7e466e1e0648a2980b056d898d2591bea7e
Michael Mallari <michael@michaelmallari>
2016-12-16 21:46:37 -0500

    Added lottery experiment

--- app_server/controllers/data.js
+++ app_server/controllers/data.js
@@ -1,3 +1,12 @@
+var request = require('request');
+var requestOptions = {
+	url: 'https://someurl.com',
+	method: "GET",
+	json: {},
+	qs: {}
+}
+
+
 // GET Data landing page - http://www.michaelmallari.com/data/
 module.exports.index = function(req, res, next) {
 	res.render('data', {
@@ -13,4 +22,39 @@ module.exports.index = function(req, res, next) {
 			}
 		}
 	});
+};
+
+
+// GET Data landing page - http://www.michaelmallari.com/data/lottery-ny-take5/
+var renderTake5 = function(req, res, responseBody) {
+	res.render('data-lottery-ny-take5', {
+		html: {
+			head: {
+				title: 'Analytics tool for Take 5 (New York State) lottery drawings',
+				description: 'With a 1:575,757 chance of winning the New York State Take 5 lottery jackpot, this tool helps you increase your odds of winning.',
+				url_path: '/data/lottery-ny-take5/'
+			},
+			body: {
+				class: 'data lottery-ny-take5',
+				url_base
+			}
+		},
+		// JSON-formatted data from NY Open Data - Take 5 winning numbers
+		drawings: responseBody
+	});
+};
+module.exports.take5 = function(req, res, next) {
+	var requestOptions;
+	requestOptions = {
+		url: 'https://data.ny.gov/resource/hh4x-xmbw.json?$$app_token=5STaLX41TI9rRUSlhpcKTgToH',
+		method: "GET",
+		json: {},
+		qs: {}
+	};
+	request(
+		requestOptions,
+		function(err, response, body) {
+			renderTake5(req, res, body);
+		}
+	);
 };
--- app_server/routes/data.js
+++ app_server/routes/data.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var ctrlData = require('../controllers/data');
 
-// GET Data landing page. - http://www.michaelmallari.com/computational/
+// GET Data landing page. - http://www.michaelmallari.com/data/
 router.get('/', ctrlData.index);
+
+// GET Lottery Take 5 landing page. - http://www.michaelmallari.com/data/lottery-ny-take5/
+router.get('/lottery-ny-take5/', ctrlData.take5);
 
 module.exports = router;
