66d126aaa583864bc7d64bd43a7f85c65df9d49d
Michael Mallari <michael@michaelmallari.com>
2017-01-27 15:17:21 -0500

    Updated data model before converting to REST API

--- app_server/controllers/design.js
+++ app_server/controllers/design.js
@@ -10,15 +10,30 @@ var requestOptions = {
 // GET Design landing page - http://www.michaelmallari.com/design/
 var renderDesign = function(req, res, responseBody) {
 	res.render('design', {
-		html: {
-			head: {
-				title: 'Design thinking + doing',
-				description: 'Done properly, design can transform businesses and be a source of competitive advantage. And the hallmark of a great designer is the ability to empathize with users through research.',
-				url_path: '/design/'
-			},
-			body: {
-				class: 'design',
-				url_base
+		meta: {
+			title: 'Design thinking + doing',
+			description: 'Done properly, design can transform businesses and be a source of competitive advantage. And the hallmark of a great designer is the ability to empathize with users through research.',
+			url_base,
+			url_path: '/design/'
+		}, 
+		content: {
+			section_hands_on: {
+				heading: 'Not affraid of getting dirty in the proverbial garage',
+				copy: 'While Michael has held cushy corporate jobs, he is also no stranger to being lean and scrappy. He has spent many hours in the “coffice”—building (coding and designing) and validating startup/product ideas. Heres a list of entrepreneurial ventures he has been involved in:',
+				logos: [
+					{  name: 'Adobe Photoshop', alias: 'photoshop'  },
+					{  name: 'Adobe Illustrator', alias: 'illustrator'  },
+					{  name: 'Lingo', alias: 'lingo' },
+					{  name: 'Sketch', alias: 'sketch' },
+					{  name: 'InVision', alias: 'invision' },
+					{  name: 'Framer', alias: 'framer' },
+					{  name: 'Flinto', alias: 'flinto' },
+					{  name: 'Zeplin', alias: 'zeplin' },
+					{  name: 'HTML 5', alias: 'html5' },
+					{  name: 'CSS 3', alias: 'css3' },
+					{  name: 'JS', alias: 'js' },
+					{  name: 'GitHUb', alias: 'github' }
+				]
 			}
 		},
 		// JSON-formatted data from Dribbble API
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,14 +1,27 @@
 // GET Home page - http://www.michaelmallari.com/
 module.exports.index = function(req, res, next) {
 	res.render('home', {
-		html: {
-			head: {
-				title: 'Hybrid thinker + doer',
-				description: 'Hybrid product manager, designer, and developer. Launched digital products for American Express, Ford, Hasbro, Marriott, Marvel, Ralph Lauren, and others.',
-				url_path: '/'
-			}, body: {
-				class: 'home',
-				url_base
+		meta: {
+			title: 'Hybrid thinker + doer',
+			description: 'Hybrid product manager, designer, and developer. Launched digital products for American Express, Ford, Hasbro, Marriott, Marvel, Ralph Lauren, and others.',
+			url_base,
+			url_path: '/'
+		}, 
+		content: {
+			section_hands_on: {
+				heading: 'Not affraid of getting dirty in the proverbial garage',
+				copy: 'While Michael has held cushy corporate jobs, he is also no stranger to being lean and scrappy. He has spent many hours in the “coffice”—building (coding and designing) and validating startup/product ideas. Heres a list of entrepreneurial ventures he has been involved in:',
+				logos: [
+					{  name: 'Credibly', alias: 'credibly'  },
+					{  name: 'Cup of Wifi', alias: 'cupofwifi'  },
+					{  name: 'Black Mamba', alias: 'blackmamba' },
+					{  name: 'Pamela Kay', alias: 'pamelakay' },
+					{  name: 'ParkingSkillz', alias: 'parkingskillz' },
+					{  name: 'Scholar Athlete Venture', alias: 'scholarathlete' },
+					{  name: 'Screech Owl', alias: 'screechowl' },
+					{  name: 'Top MBA Applicants', alias: 'topmbaapplicants' },
+					{  name: 'WorkLifeEdge', alias: 'worklifeedge' }
+				]
 			}
 		}
 	});
@@ -21,15 +34,6 @@ module.exports.index = function(req, res, next) {
 // GET About page - http://www.michaelmallari.com/about/
 module.exports.about = function(req, res, next) {
 	res.render('default', {
-		html: {
-			head: {
-				title: 'About',
-				description: 'Michael leverages over 15 years of product, design, and engineering experience to deliver delightful digital customer experiences that align with business goals.',
-				url_path: '/about/'
-			}, body: {
-				class: 'default',
-				url_base
-			}
-		}
+
 	});
 };
