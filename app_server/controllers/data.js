6dc9cf8f21b328052268c62bfbb89fd7fb2f6b74
Michael Mallari <michael@michaelmallari.com>
2017-01-28 16:53:34 -0500

    Added assets

--- app_server/controllers/data.js
+++ app_server/controllers/data.js
@@ -9,15 +9,12 @@ var section_hands_on = {
 	heading: 'Not affraid of getting dirty in the proverbial garage',
 	copy: 'While Michael has held cushy corporate jobs, he is also no stranger to being lean and scrappy. He has spent many hours in the “coffice”—building (coding and designing) and validating startup/product ideas. Heres a list of entrepreneurial ventures he has been involved in:',
 	logos: [
-		{  name: 'Credibly', alias: 'credibly'  },
-		{  name: 'Cup of Wifi', alias: 'cupofwifi'  },
-		{  name: 'Black Mamba', alias: 'blackmamba' },
-		{  name: 'Pamela Kay', alias: 'pamelakay' },
-		{  name: 'ParkingSkillz', alias: 'parkingskillz' },
-		{  name: 'Scholar Athlete Venture', alias: 'scholarathlete' },
-		{  name: 'Screech Owl', alias: 'screechowl' },
-		{  name: 'Top MBA Applicants', alias: 'topmbaapplicants' },
-		{  name: 'WorkLifeEdge', alias: 'worklifeedge' }
+		{  name: 'SQL', alias: 'sql' },
+		{  name: 'Python', alias: 'python' },
+		{  name: 'NumPy', alias: 'numpy' },
+		{  name: 'SciPy', alias: 'scipy' },
+		{  name: 'TensorFlow', alias: 'tensorflow' },
+		{  name: 'D3', alias: 'd3'  }
 	]
 }
 
