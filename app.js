5bd7010cc48755e93f58e1cce82b7fea617715f1
Michael Mallari <michael@michaelmallari.com>
2017-01-27 23:20:57 -0500

    Tweaked template media queries

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var bodyParser = require('body-parser');
 // require('./app_server/models/db');
 
 var mainRoutes = require('./app_server/routes/main');
+// var designRoutes = require('./app_server/routes/design');
 // var computationalRoutes = require('./app_server/routes/computational');
 // var dataRoutes = require('./app_server/routes/data');
-// var designRoutes = require('./app_server/routes/design');
+
 
 if(process.env.NODE_ENV === 'production') {
 	url_base = 'http://www.michaelmallari.com';
@@ -36,9 +37,9 @@ app.use('/node_modules/jquery', express.static(__dirname + '/node_modules/jquery
 app.use('/node_modules/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.use('/', mainRoutes);
+// app.use('/design', designRoutes);
 // app.use('/computational', computationalRoutes);
 // app.use('/data', dataRoutes);
-// app.use('/design', designRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
