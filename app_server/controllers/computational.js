7cc58856604d0c18294a7db6080dfcd8920c0c47
Michael Mallari <michael@michaelmallari.com>
2017-01-28 16:46:04 -0500

    Updated templates

--- app_server/controllers/computational.js
+++ app_server/controllers/computational.js
@@ -1,19 +1,16 @@
 var section_hands_on = {
 	heading: 'Geeking out with modern web and mobile technologies',
-	copy: 'With an undergraduate degree in Computer Science from New York Institute of Technology\'s School of Engineering and Computing Sciences and over ten years of professional software engineering experience, Michael has been (and still is) a techie at heart. Here\'s an abbreviated list of technologies he codes with/uses to deploy digital solutions:',
+	copy: 'With an undergraduate degree in Computer Science from New York Institute of Technology\'s School of Engineering and Computing Sciences and over ten years of professional front-end development experience, Michael has been (and still is) a techie at heart. Here\'s an abbreviated list of technologies he codes with/uses to deploy digital solutions:',
 	logos: [
 		{  name: 'HTML5', alias: 'html5'  },
 		{  name: 'CSS3', alias: 'css3'  },
-		{  name: 'LESS', alias: 'less' },
 		{  name: 'JavaScript', alias: 'js' },
 		{  name: 'Node.js', alias: 'nodejs' },
 		{  name: 'Python', alias: 'python' },
-		{  name: 'Flask', alias: 'flask' },
 		{  name: 'PHP', alias: 'php' },
 		{  name: 'Angular', alias: 'angular' },
 		{  name: 'NativeScript', alias: 'nativescript' },
 		{  name: 'React', alias: 'react' },
-		{  name: 'React Native', alias: 'reactnative' },
 		{  name: 'GitHub', alias: 'github' },
 		{  name: 'AWS', alias: 'aws' },
 		{  name: 'Heroku', alias: 'Heroku' }
