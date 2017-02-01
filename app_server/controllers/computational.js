7066370de5813386fc3d13302aca7e9d2a6a4f39
Michael Mallari <michael@michaelmallari.com>
2017-02-01 18:52:39 -0500

    Updated styles

--- app_server/controllers/computational.js
+++ app_server/controllers/computational.js
@@ -1,19 +1,19 @@
 var section_hands_on = {
-	heading: 'Geeking out with modern web and mobile technologies',
+	heading: 'Geeking out with modern computing technologies.',
 	copy: 'With an undergraduate degree in Computer Science from New York Institute of Technology\'s School of Engineering and Computing Sciences and over ten years of professional software engineering experience, Michael has been (and still is) a techie at heart. Here\'s an abbreviated list of technologies he codes with/uses to deploy digital solutions:',
 	logos: [
-		{  name: 'HTML5', alias: 'html5', url: ''  },
-		{  name: 'CSS3', alias: 'css3', url: ''  },
-		{  name: 'JavaScript', alias: 'js', url: '' },
-		{  name: 'Node.js', alias: 'nodejs', url: '' },
-		{  name: 'Python', alias: 'python', url: '' },
+		{  name: 'JavaScript', alias: 'js' },
+		{  name: 'Node.js', alias: 'nodejs' },
+		{  name: 'Angular', alias: 'angular' },
+		{  name: 'NativeScript', alias: 'nativescript' },
+		{  name: 'React', alias: 'react' },
+		{  name: 'Python', alias: 'python' },
+		{  name: 'TensorFlow', alias: 'tensorflow' },
 		{  name: 'PHP', alias: 'php', url: '' },
-		{  name: 'Angular', alias: 'angular', url: '' },
-		{  name: 'NativeScript', alias: 'nativescript', url: '' },
-		{  name: 'React', alias: 'react', url: '' },
-		{  name: 'Git', alias: 'git', url: '' },
-		{  name: 'AWS', alias: 'aws', url: '' },
-		{  name: 'Heroku', alias: 'heroku', url: '' }
+		{  name: 'Git', alias: 'git' },
+		{  name: 'GitHub', alias: 'github' },
+		{  name: 'AWS', alias: 'aws' },
+		{  name: 'Heroku', alias: 'heroku' }
 	]
 }
 
